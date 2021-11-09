ad96deb7270796d389ea5314b0ec9ae58b86c567
Eduardo-LP <eduardolemospedroso@gmail.com>
2021-11-09 09:28:09 -0300

    conclusão do exercicio listagem de inteiros com lambda

--- src/App_ListaInteiros.java
+++ src/App_ListaInteiros.java
@@ -6,11 +6,19 @@ public class App_ListaInteiros {
     public static ArrayList<Integer> inteiros = new ArrayList<>();
 
     public static void main(String[] args) {
+        ListaInteiros listaInteiros = new ListaInteiros();
         inteiros.add(10);
         inteiros.add(20);
         inteiros.add(30);
         inteiros.add(40);
-
-        new ListaInteiros().listagem(inteiros, (int g, int p) -> g+p);
+        inteiros.add(5);
+        inteiros.add(80);
+        inteiros.add(4);
+        System.out.println("---- Retorna nova lista----");
+        listaInteiros.listagem(inteiros, Integer::sum);
+        System.out.println("----Returna media----");
+        listaInteiros.listagem(inteiros,(int g, int p) -> (g+p)/2);
+        System.out.println("----Retorna o maior valor em pares----");
+        listaInteiros.listagem(inteiros, Math::max);
     }
 }
--- src/services/ListaInteiros.java
+++ src/services/ListaInteiros.java
@@ -8,7 +8,7 @@ public class ListaInteiros {
     public ArrayList<Integer> listagem(ArrayList<Integer> listaInteiros, Inteiros inteiros){
         ArrayList<Integer> newList = new ArrayList<>();
         for(int i = 0; i < listaInteiros.size(); i++) {
-            if (i == 3) break;
+            if (i == (listaInteiros.size()-1)) break;
             newList.add(inteiros.nome(listaInteiros.get(i), listaInteiros.get(i+1)));
             System.out.println(inteiros.nome(listaInteiros.get(i), listaInteiros.get(i+1)));
         }
